fcd7eb82ac048e3746a9cef8e3aa6af482d63da2
aidanr67 <aidanr67@gmail.com>
2015-11-13 08:09:14 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+/********************************************************************
+//Config info for prosody admin web interface
+/*******************************************************************/
+$DBhost = 'localhost';
+$DBuser = 'prosody';
+$DBpassword = 'your_password';
+$DBdatabase = 'prosody';
+?>
