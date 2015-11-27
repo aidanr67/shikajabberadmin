5d2997f51aee6d200e7f053e85679d6b880c088b
aidanr67 <aidanr67@gmail.com>
2015-11-27 18:08:31 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,16 @@
 /********************************************************************
 //Config info for prosody admin web interface
 /*******************************************************************/
+$file = $_SERVER['DOCUMENT_ROOT'] . "/../dbpass.txt";
+if (file_exists($file)){
+    $fh = fopen($file, r);
+    $DBpassword = fgets($fh);
+    fclose($fh);
+} else {
+    die("No DB password provided");
+}
+$DBpassword = rtrim($DBpassword);
 $DBhost = 'localhost';
 $DBuser = 'prosody';
-$DBpassword = 'your_password';
 $DBdatabase = 'prosody';
 ?>
